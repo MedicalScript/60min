23c04a80ae32868385a4c489171f77e356aab4c9
MedicalScript <medicalscript.soporte@gmail.com>
2026-03-30 18:33:17 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-self.addEventListener('install', (e) => {
-    console.log('[Service Worker] Install');
+const CACHE_NAME = 'devocional-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-    // Para esta versión básica, simplemente dejamos que haga las peticiones normales a internet.
-    // Esto es lo mínimo requerido para que Chrome ofrezca la opción de instalar.
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Si el archivo ya está en la memoria del teléfono, lo saca de ahí. Si no, lo busca en internet.
+                return response || fetch(event.request);
+            })
+    );
 });
